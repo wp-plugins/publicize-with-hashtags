c389a0d12a59e585bab59cc641c80bf3d58b885c
Neilgaiman <Neilgaiman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-11 18:16:13 +0000

    Added license.txt, README.md and fixed some typo in the readme.txt file.
    git-svn-id: https://plugins.svn.wordpress.org/publicize-with-hashtags/trunk@1218434 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- publicize-with-hashtags.php
+++ publicize-with-hashtags.php
@@ -1,11 +1,28 @@
 <?php
 /*
 Plugin Name: Publicize With Hashtags
+Plugin URI: http://www.ryadel.com/works/publicize-with-hashtags/
 Description: Automatically append hashtags to any content sent by Jetpack's Publicize module: hashtags will be created using post tags: dupe check and an optional length-based threshold are also included.
 Version: 0.1.2
-License: GPL
+License: GPLv2 or later
 Author: Darkseal
 Author URI: http://www.ryadel.com/
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 // If Jetpack is not installed or Publicize module isn't active, just exit now.
